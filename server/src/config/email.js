663efe13ae52430096d05571bca43f2cf2e39ba6
Sahileweb <sahil2003mundhe@gmail.com>
2026-09-19 20:10:52 +0530

    email error

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -3,23 +3,14 @@
 const nodemailer = require('nodemailer')
 
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.EMAIL_USER,
-//     pass: process.env.EMAIL_PASS   
-//   }
-// })
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+    pass: process.env.EMAIL_PASS   
+  }
+})
+
 
 transporter.verify((error) => {
   if (error) {
